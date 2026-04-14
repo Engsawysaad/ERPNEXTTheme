95db089cbd148817e22568ac0381167fd17dcd1b
Engsawysaad <engsaad@example.com>
2026-04-14 13:30:34 +0300

    Simplify webpack.config.js

--- erpnext_custom_theme/webpack.config.js
+++ erpnext_custom_theme/webpack.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  // Theme app - no JS to bundle, CSS is handled via hooks.py
-  js: null,
-  css: null,
-  app_include_js: {},
-  boot_js: {},
+  theme: {
+    no_blur: false,
+  },
 };
